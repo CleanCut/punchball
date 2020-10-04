959d778d80050ead17c9b8b94444e1934c2c9a0d
Nathan Stocks <cleancut@github.com>
2020-10-04 09:14:35 -0600

    Stopped spawning a new, overlapping arena every frame. Silly me. Thanks to  in Discord for pointing it out.

--- src/arena.rs
+++ src/arena.rs
@@ -20,7 +20,7 @@ impl Plugin for ArenaPlugin {
         app.add_resource(LeaveArenaDebouncer {
             player_timers: HashMap::with_capacity(4),
         })
-        .add_system(spawn_arena_system.system())
+        .add_startup_system(spawn_arena_system.system())
         .add_system(leave_arena_system.system());
     }
 }
@@ -60,25 +60,22 @@ fn leave_arena_system(
     players: Query<&Player>,
     arena: Query<&Arena>,
 ) {
-    let mut leaving_players = HashSet::new();
-    let mut i = 0;
     while let Ok(proximity_event) = physics_events.proximity_events.pop() {
         if proximity_event.new_status == Proximity::Disjoint {
             let e1 = *(bh_to_e.0.get(&proximity_event.collider1).unwrap());
             let e2 = *(bh_to_e.0.get(&proximity_event.collider2).unwrap());
             if players.get::<Player>(e1).is_ok() && arena.get::<Arena>(e2).is_ok() {
-                leaving_players.insert(players.get::<Player>(e1).unwrap().id);
+                println!(
+                    "Player {} left the arena",
+                    players.get::<Player>(e1).unwrap().id
+                );
             }
             if players.get::<Player>(e2).is_ok() && arena.get::<Arena>(e1).is_ok() {
-                leaving_players.insert(players.get::<Player>(e2).unwrap().id);
+                println!(
+                    "Player {} left the arena",
+                    players.get::<Player>(e2).unwrap().id
+                );
             }
         }
-        i += 1;
-    }
-    if i > 0 {
-        println!("{}", i);
-    }
-    for id in leaving_players {
-        println!("Player {} left the arena", id);
     }
 }
