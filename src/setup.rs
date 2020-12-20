8f78e60e6dfe708bd4ae959b072d24bf30f292cc
Nathan Stocks <cleancut@github.com>
2020-12-20 11:46:58 -0700

    Update to Bevy 0.4.0

--- src/setup.rs
+++ src/setup.rs
@@ -1,19 +1,20 @@
 use bevy::prelude::*;
 #[derive(Default)]
 pub struct SetupPlugin;
+
 impl Plugin for SetupPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
             .add_resource(WindowDescriptor {
                 title: "Punch Ball".to_string(),
-                width: 1024,
-                height: 1024,
+                width: 1024.0,
+                height: 1024.0,
                 vsync: true,
                 resizable: false,
                 cursor_locked: false,
                 ..Default::default()
             })
-            .add_startup_system(setup);
+            .add_startup_system(setup.system());
     }
 }
 
